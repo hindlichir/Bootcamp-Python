9b3b1193088fb448c6949f54bd8ddd33970879cf
Hind <hlichir@icloud.com>
2021-11-10 10:26:39 +0100

    small modification

--- Module00/ex05/kata04.py
+++ Module00/ex05/kata04.py
@@ -1,6 +1,4 @@
-from decimal import Decimal
-
 t = (0, 4, 132.42222, 10000, 12345.67)
 
-print("module_{0[0]:02}, ex_{0[1]:02} : {0[2]:.2f}".format(t), end="")
-print("{1:.2E}, {2:.2E}".format(float(t[3]), float(t[4])))
+print("module_{0[0]:02}, ex_{0[1]:02} : {0[2]:.2f}, ".format(t), end="")
+print("{0:.2E}, {1:.2E}".format(float(t[3]), float(t[4])))
