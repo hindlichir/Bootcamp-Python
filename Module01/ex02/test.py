fcc1a618e3eb32cecf5a8495239df4c653b6244a
Hind LICHIR <hlichir@e2r5p18.clusters.42paris.fr>
2021-11-10 17:52:50 +0100

    ex02 done

--- Module01/ex02/test.py
+++ Module01/ex02/test.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from vector import Vector
 
 print()
@@ -16,6 +17,9 @@ print(test.shape)
 test2 = Vector([[0.0], [1.0], [2.0], [3.0]])
 print()
 
+test = Vector([[0.0], [1.0], [2.0], [3.0]])
+test2 = Vector([[0.0], [1.0], [2.0], [3.0]])
+
 res = test + test2
 print(res)
 print(res.values)
@@ -26,6 +30,72 @@ test = Vector([0.0, 1.0, 2.0, 3.0])
 test2 = Vector([0.0, 1.0, 2.0, 3.0])
 
 res = test + test2
+print(res)
+print(res.values)
+print(res.shape)
+print()
+
+test = Vector([[0.0], [1.0], [2.0], [3.0]])
+test2 = Vector([[0.0], [1.0], [2.0], [3.0]])
+
+res = test - test2
+print(res)
+print(res.values)
+print(res.shape)
+print()
+
+test = Vector([0.0, 1.0, 2.0, 3.0])
+test2 = Vector([0.0, 1.0, 2.0, 3.0])
+
+res = test - test2
+print(res)
+print(res.values)
+print(res.shape)
+print()
+
+test = Vector([[0.0], [1.0], [2.0], [3.0]])
+test2 = Vector([[0.0], [1.0], [2.0], [3.0]])
+
+res = test / 2
+res = test2 / 2.0
+print(res)
+print(res.values)
+print(res.shape)
+print()
+
+test = Vector([0.0, 1.0, 2.0, 3.0])
+test2 = Vector([0.0, 1.0, 2.0, 3.0])
+
+res = test / 2
+res = test2 / 2.0
+print(res)
+print(res.values)
+print(res.shape)
+print()
+
+test = Vector([[0.0], [1.0], [2.0], [3.0]])
+test2 = Vector([[0.0], [1.0], [2.0], [3.0]])
+
+res = test * 2.0
+print(test)
+print(res.values)
+print(res.shape)
+print()
+res = test2 * 0
+print(test)
+print(res.values)
+print(res.shape)
+print()
+
+test = Vector([0.0, 1.0, 2.0, 3.0])
+test2 = Vector([0.0, 1.0, 2.0, 3.0])
+
+res = test * 2.0
+print(res)
+print(res.values)
+print(res.shape)
+print()
+res = test2 * 0
 print(res)
 print(res.values)
 print(res.shape)
--- Module01/ex02/vector.py
+++ Module01/ex02/vector.py
@@ -16,42 +16,51 @@ class Vector:
         return self._values
 
     @values.setter
-    def values(self, values):
+    def values(self, v):
         lst = []
         s = "The argument can either be a list, "
         s = s + "a list of list of floats, a size or a range"
-        if isinstance(values, int):
-            for i in range(0, values):
+        check_tuple = isinstance(v, tuple) and len(v) == 2
+        if isinstance(v, int):
+            for i in range(0, v):
                 lst.append([float(i)])
             self._values = lst
-        elif isinstance(values, tuple) and len(values) == 2:
-            for i in range(values[0], values[1]):
+        elif check_tuple and isinstance(v[0], int) and isinstance(v[0], int):
+            for i in range(v[0], v[1]):
                 lst.append([float(i)])
             self._values = lst
-        elif isinstance(values, list):
-            if (len(values) == 0):
+        elif isinstance(v, list):
+            if (len(v) == 0):
                 raise ValueError("The vector can't be an empty list")
             else:
                 error = 0
-                if isinstance(values[0], list) and len(values[0]) == 1:
-                    for elem in values:
+                if isinstance(v[0], list) and len(v[0]) == 1:
+                    for elem in v:
                         check = not isinstance(elem, list) or len(elem) != 1
                         if check or not isinstance(elem[0], float):
                             error = 1
                             break
-                elif isinstance(values[0], float):
-                    for elem in values:
+                elif isinstance(v[0], float):
+                    for elem in v:
                         if not isinstance(elem, float):
                             error = 1
                             break
                 else:
                     error = 1
                 if error == 0:
-                    self._values = values
+                    self._values = v
                 else:
                     raise ValueError(s)
         else:
             raise ValueError(s)
+
+    def __str__(self):
+        s = f"The vector is {self._values} of shape {self.shape}."
+        return s
+
+    def __repr__(self):
+        s = f"Vector(values={self._values}, shape={self.shape})"
+        return s
 
     def __add__(self, other):
         if self.shape == other.shape:
@@ -61,8 +70,69 @@ class Vector:
                     new.values[i][0] = new.values[i][0] + other.values[i][0]
             else:
                 for i in range(0, len(new.values)):
-                new.values[i] = new.values[i] + other.values[i]
+                    new.values[i] = new.values[i] + other.values[i]
             return new
         else:
             raise ValueError("Vectors need to be the same shape")
 
+    def __radd__(self, other):
+        if other == 0:
+            return self
+        else:
+            return self.__add__(other)
+
+    def __sub__(self, other):
+        if self.shape == other.shape:
+            new = Vector(self._values)
+            if isinstance(new.values[0], list):
+                for i in range(0, len(new.values)):
+                    new.values[i][0] = new.values[i][0] - other.values[i][0]
+            else:
+                for i in range(0, len(new.values)):
+                    new.values[i] = new.values[i] - other.values[i]
+            return new
+        else:
+            raise ValueError("Vectors need to be the same shape")
+
+    def __rsub__(self, other):
+        if other == 0:
+            return self
+        else:
+            return self.__sub__(other)
+
+    def __truediv__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            if other == 0:
+                raise ValueError("You cannot divide by zero!")
+            else:
+                new = Vector(self._values)
+                if isinstance(new.values[0], list):
+                    for i in range(0, len(new.values)):
+                        new.values[i][0] = new.values[i][0] / other
+                else:
+                    for i in range(0, len(new.values)):
+                        new.values[i] = new.values[i] / other
+                return new
+        else:
+            raise ValueError("Vectors can only divided by a scalar")
+
+    def __rtruediv__(self, other):
+        if not isinstance(other, int) and not isinstance(other, float):
+            raise ValueError("What are you trying to do?")
+        raise ValueError("A scalar cannot be divided by a Vector")
+
+    def __mul__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            new = Vector(self._values)
+            if isinstance(new.values[0], list):
+                for i in range(0, len(new.values)):
+                    new.values[i][0] = new.values[i][0] * other
+            else:
+                for i in range(0, len(new.values)):
+                    new.values[i] = new.values[i] * other
+            return new
+        else:
+            raise ValueError("Vectors can only divided by a scalar")
+
+    def __rmul__(self, other):
+        return self.__mult__(other)
