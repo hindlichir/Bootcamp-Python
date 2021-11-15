c1f92d73e053868d73e9adb90f15592cb894834c
Hind LICHIR <hlichir@e2r8p17.clusters.42paris.fr>
2021-11-15 14:05:37 +0100

    ex02 done

--- Module03/ex02/ScrapBooker.py
+++ Module03/ex02/ScrapBooker.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class ScrapBooker:
 
-    def crop(self, array, dim, pos=(0,0)):
+    def crop(self, array, dim, pos=(0, 0)):
         if not type(array).__module__ == np.__name__:
             return None
         if not isinstance(dim, tuple) or len(dim) != 2:
@@ -28,32 +28,19 @@ class ScrapBooker:
             return None
         if axis == 0 and n <= array.shape[1]:
             p = n - 1
-            range_x = range(0, array.shape[0])
-            range_y = range(0, array.shape[1])
-            matrix = [[array[x][y] for y in range_y if y != p] for x in range_x]
+            ran_x = range(0, array.shape[0])
+            ran_y = range(0, array.shape[1])
+            matrix = [[array[x][y] for y in ran_y if y != p] for x in ran_x]
             return np.array(matrix)
         elif axis == 1 and n <= array.shape[0]:
             p = n - 1
-            range_x = range(0, array.shape[0])
-            range_y = range(0, array.shape[1])
-            matrix = [[array[x][y] for y in range_y] for x in range_x if x != p]
+            ran_x = range(0, array.shape[0])
+            ran_y = range(0, array.shape[1])
+            matrix = [[array[x][y] for y in ran_y] for x in ran_x if x != p]
             return np.array(matrix)
-        return None      
-            
+        return None
 
     def juxtapose(self, array, n, axis):
-        """
-        Juxtaposes n copies of the image along the specified axis.
-        Args:
-        array: numpy.ndarray.
-        n: positive non null integer.
-        axis: integer of value 0 or 1.
-        Returns:
-        new_arr: juxtaposed numpy.ndarray.
-        None otherwise (combinaison of parameters not incompatible).
-        Raises:
-        This function should not raise any Exception.
-        """
         if not type(array).__module__ == np.__name__:
             return None
         if not isinstance(n, int) or n <= 0:
@@ -63,44 +50,31 @@ class ScrapBooker:
         if axis == 0:
             range_x = range(0, array.shape[0])
             range_y = range(0, array.shape[1])
-            range_i = range(0, array.shape[1] * n)
             matrix = []
-            for x in range_x:
-                for i, y in range_i, range_y:
-                    matrix[x][i * n + y] = array[x][y]
+            for i in range(0, n):
+                for x in range_x:
+                    matrix.append([array[x][y] for y in range_y])
             return np.array(matrix)
         elif axis == 1:
             range_x = range(0, array.shape[0])
             range_y = range(0, array.shape[1])
-            range_i = range(0, array.shape[1] * n)
             matrix = []
             for x in range_x:
-                for i in range(0, n):
-                    for y in range_y:
-                        matrix[i * n + x][y] = array[x][y]
+                matrix.append([array[x][y] for y in range_y] * n)
             return np.array(matrix)
         return None
 
     def mosaic(self, array, dim):
-        """
-        Makes a grid with multiple copies of the array. The dim argument specifies
-        the number of repetition along each dimensions.
-        Args:
-        array: numpy.ndarray.
-        dim: tuple of 2 integers.
-        Returns:
-        new_arr: mosaic numpy.ndarray.
-        None otherwise (combinaison of parameters not incompatible).
-        Raises:
-        This function should not raise any Exception.
-        """
-
-spb = ScrapBooker()
-arr1 = np.arange(0,25).reshape(5,5)
-print(spb.crop(arr1, (3,1),(1,0)))
-
-arr2 = np.array("A B C D E F G H I".split() * 6).reshape(-1, 9)
-print(spb.thin(arr2,3,0))
-
-arr3 = np.array([[1, 2, 3],[1, 2, 3],[1, 2, 3]])
-print(spb.juxtapose(arr3, 3, 1))
+        if not type(array).__module__ == np.__name__:
+            return None
+        if not isinstance(dim, tuple) or len(dim) != 2:
+            return None
+        elif not isinstance(dim[0], int) or not isinstance(dim[1], int):
+            return None
+        matrix = []
+        range_x = range(0, array.shape[0])
+        range_y = range(0, array.shape[1])
+        for i in range(0, dim[1]):
+            for x in range_x:
+                matrix.append([array[x][y] for y in range_y] * dim[0])
+        return np.array(matrix)
